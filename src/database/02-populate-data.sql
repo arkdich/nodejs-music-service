26b6f102a633670cba316c2ca04501f5d4975c4d
Artemy Dyachuk <artem002498@gmail.com>
2024-05-27 23:27:38 +0500

    build: postgre image & dockerfile

---
+++ src/database/02-populate-data.sql
@@ -0,0 +1,5 @@
+\c music_service
+
+insert into public.artist (name,grammy) values ('Twenty One Pilots',true);
+insert into public.artist (name,grammy) values ('Taylor Swift',true);
+insert into public.artist (name,grammy) values ('The Neighbourhood',true);
