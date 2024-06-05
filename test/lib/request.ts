4d499cae89f8c694c7764b8efb7c8afbf4d86b14
Artemy Dyachuk <artem002498@gmail.com>
2024-06-05 22:51:08 +0500

    chore: rm outdated tests

--- test/lib/request.ts
+++
@@ -1,8 +0,0 @@
-import * as request from 'supertest';
-import 'dotenv/config';
-import { PORT_API } from 'src/app';
-
-const host = `localhost:${PORT_API}`;
-const _request = request(host);
-
-export default _request;
