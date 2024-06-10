2405c2aa99393e174177801a1734d878d8ef7eb6
Artemy Dyachuk <artem002498@gmail.com>
2024-06-10 19:35:08 +0500

    add: swagger json route

--- src/setup-swagger.ts
+++ src/setup-swagger.ts
@@ -1,5 +1,6 @@
 import { INestApplication } from '@nestjs/common';
 import { SwaggerModule } from '@nestjs/swagger';
+import { Request, Response } from 'express';
 import { readFileSync } from 'fs';
 import * as path from 'path';
 import { parse } from 'yaml';
@@ -9,6 +10,10 @@ export const setupSwagger = (app: INestApplication) => {
 
   const apiYaml = readFileSync(filePath, 'utf8');
   const document = parse(apiYaml);
+
+  app.use('api-docs-json', (_: Request, res: Response) => {
+    res.json(document);
+  });
 
   SwaggerModule.setup('api-docs', app, document, {
     swaggerOptions: { tryItOutEnabled: true, persistAuthorization: true },
